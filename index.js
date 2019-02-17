dde0dc86439dca37568e49f3916b03bfac3a6f7c
Christopher Light <light.christopher@gmail.com>
2019-02-17 11:58:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   console.log(`hello`)
   return `I can't hear you!`
+}
 
+function sayHiToGrandma(string) {
   console.log('HELLO')
   return `YES INDEED!`
 }
